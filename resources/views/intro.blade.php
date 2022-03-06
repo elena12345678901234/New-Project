d441b64857af6dfd629240b2ee807457e2048d1d
elena12345678901234 <89983951+elena12345678901234@users.noreply.github.com>
2022-03-06 13:51:23 +0200

    header, footer and login page done

---
+++ resources/views/intro.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel= 'stylesheet' href='css.css'>
+    <title>Introducere Laravel</title>
+</head>
+<body style = background-color:aqua>
+    <h1>Acesta este primul curs Laravel!</h1>
+    <p>Mult succes!!!! :)</p>
+   
+    <a href="https://laravel.com/docs/4.2/introduction">Intro</a>
+    <br/>
+    <a href="https://laravel.com/docs/8.x/installation">Installation</a>
+    <br/>
+    <a href="https://laravel.com/docs/8.x/readme">Documentation</a>
+    <br/>
+    <br/>
+    <h2 style = color:crimson >What is Laravel and why it is used?</h2>
+    <br/>
+    <p>Laravel is a web application framework with expressive, elegant syntax. ... Laravel attempts to take the pain out of development by easing common tasks used in the majority of web projects, such as authentication, routing, sessions, and caching.</p>
+    <br/>
+    <br/>
+    <h2 style = color:crimson >Is Laravel a programming language?</h2>
+    <p>Laravel is a framework built using PHP – an open-source programming language that has been at the forefront of the most popular backend languages for years</p>
+    <br/>
+    <br/>
+
+</body>
+</html>
